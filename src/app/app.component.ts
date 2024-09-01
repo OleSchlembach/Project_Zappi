144851a0a07b3cde3459b996b2f3a80ace37fdc1
Ole Schlembach <ole.schlembach1510@web.de>
2024-09-01 10:23:12 +0200

    DurchschnittlicherStromverbrauchComponent hinzugefügt mit anzeige eines gemockten Stromverbrauchs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {
+  DurchschnittlicherStromverbrauchComponent
+} from "./durchschnittlicher-stromverbrauch/durchschnittlicher-stromverbrauch.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DurchschnittlicherStromverbrauchComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
