1dcb216e07007076e8b980a7c99cc55b5459afdd
Ole Schlembach <ole.schlembach1510@web.de>
2024-09-02 10:54:12 +0200

    ContactScreen hinzugefügt Switchfunktionen um zwischen den seinten zu schwitchen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {
-  DurchschnittlicherStromverbrauchComponent
-} from "./durchschnittlicher-stromverbrauch/durchschnittlicher-stromverbrauch.component";
+import { DurchschnittlicherStromverbrauchComponent } from "./durchschnittlicher-stromverbrauch/durchschnittlicher-stromverbrauch.component";
+import { HomeScreenComponent } from "./home-screen/home-screen.component";
+import { ContactScreenComponent } from "./contact-screen/contact-screen.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DurchschnittlicherStromverbrauchComponent],
+  imports: [RouterOutlet, DurchschnittlicherStromverbrauchComponent, HomeScreenComponent, ContactScreenComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Zappi';
+  showHomeScreen = true;
+  showContactsScreen = false;
+
+  switchToContactScreen(){
+    this.toggleHomeScreen();
+    this.toggleContactScreen();
+  }
+
+  toggleHomeScreen(){
+    this.showHomeScreen = !this.showHomeScreen;
+  }
+  toggleContactScreen(){
+    this.showContactsScreen = !this.showContactsScreen;
+  }
 }
+
+
---
+++ src/app/contact-screen/contact-screen.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-screen',
+  standalone: true,
+  imports: [],
+  templateUrl: './contact-screen.component.html',
+  styleUrl: './contact-screen.component.css'
+})
+export class ContactScreenComponent {
+
+}
