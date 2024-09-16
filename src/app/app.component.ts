acaae78966f34f7f7f2adeb43df6938687589b29
Ole Schlembach <ole.schlembach1510@web.de>
2024-09-16 10:16:34 +0200

    lösch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { DurchschnittlicherStromverbrauchComponent } from "./durchschnittlicher-stromverbrauch/durchschnittlicher-stromverbrauch.component";
 import { HomeScreenComponent } from "./home-screen/home-screen.component";
-import { ContactScreenComponent } from "./contact-screen/contact-screen.component";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DurchschnittlicherStromverbrauchComponent, HomeScreenComponent, ContactScreenComponent],
+  imports: [RouterOutlet, HomeScreenComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
